d3f578e35d888b29a77d7a524826515148e8f9f2
Bartan89 <bart.kuijper89@gmail.com>
2020-10-13 13:00:39 +0200

    first steps with scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,60 @@
+import scala.io.StdIn.{readInt, readLine}
+
 object Main extends App {
 
 
 
-  val exampleList = List(1,4,5,7,8,10,1,0)
 
-  val something: List[Int] = exampleList.filter(x => x > 3 && x < 8)
 
-  println(something)
+
+def programLoop(): Unit = {
+
+  print("What would you like to do?\n")
+  print("1. Age thingie\n")
+  print("2. stop program\n")
+  val menu = readInt()
+
+  if(menu == 1){
+
+
+
+
+  print("How old are you? ")
+  val age = readInt()
+
+
+  val birthday = age match {
+    case 0 => "first"
+    case 1 => "second"
+    case 2 => "third"
+    case _ => age + "th"
+  }
+
+  println(s"it is your $birthday birthday..")
+
+  //conditionals
+  if(age >= 18) println("you can learn to drive")
+
+  //  val exampleList = List(1,4,5,7,8,10,1,0)
+  //
+  //  val something: List[Int] = exampleList.filter(x => x > 4 && x < 8)
+  //
+  ////
+  //  print
+    programLoop()
+  } else {
+    println("bye bye\n")
+  }
+}
+
+  programLoop()
+
+
+
+
+
+
+
 
 }
 
